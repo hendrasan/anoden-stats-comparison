934977e2688be8bdaea411c3d4800a0f86e91cbe
Hendra Susanto <anotherhendrasusanto@gmail.com>
2025-04-15 14:58:30 +0700

    chore: update web title and description

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import CharacterComparison from "@/components/character-comparison"
 export default function Home() {
   return (
     <main className="container py-10 px-4 mx-auto">
-      <h1 className="mb-8 text-3xl font-bold text-center">Character Comparison</h1>
+      <h1 className="mb-8 text-3xl font-bold text-center">Another Eden Character Stats Comparison</h1>
       <CharacterComparison />
     </main>
   );
